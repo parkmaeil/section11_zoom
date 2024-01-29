5efcb380e299487485cfdd3828907e3ce6d51df0
bitcocom <bitcocom@empas.com>
2024-01-29 16:47:50 +0900

    restful

--- src/main/java/com/example/controller/TemplateController.java
+++ src/main/java/com/example/controller/TemplateController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TemplateController {
-
     @RequestMapping("/test")
     public String test(){
          return "Restful API Test";
